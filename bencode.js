9c6cd1eaebdb4861722d568cf5eb8b9ad3ab8abd
Jack Palevich <jackpal@google.com>
2010-05-16 11:47:51 +0800

    Stylistic changes to pass JSLint.

--- bencode.js
+++ bencode.js
@@ -1,101 +1,120 @@
 exports.encode = function encode(s) {
-	var type = typeof s;
-	if (type === 'number') {
-		return 'i' + s + 'e';
-	} else if (type === 'string') {
-		return s.length + ':' + s;
-	} else if (type === 'object') {
-		if (s) {
-			if (s instanceof Array) {
-				var result = 'l';
-				for (var i = 0, sLen = s.length; i < sLen; i++) {
-					result += encode(s[i]);
-				}
-				return result + 'e';
-			} else {
-				var result = 'd';
-				for(var prop in s) {
-				    if(s.hasOwnProperty(prop)) {
-				        result += encode(prop) + encode(s[prop]);
-					}
-				}
-				return result + 'e';
-			}
-		}
-		throw "unexpected null";
-	} else {
-		throw "unexpected type " + type;
-	}
-}
+    var type = typeof s, result, i, sLen, prop;
+    if (type === 'number') {
+        return 'i' + s + 'e';
+    } else if (type === 'string') {
+        return s.length + ':' + s;
+    } else if (type === 'object') {
+        if (s) {
+            if (s instanceof Array) {
+                result = 'l';
+                for (i = 0, sLen = s.length; i < sLen; i += 1) {
+                    result += encode(s[i]);
+                }
+                return result + 'e';
+            } else {
+                result = 'd';
+                for (prop in s) {
+                    if (s.hasOwnProperty(prop)) {
+                        result += encode(prop) + encode(s[prop]);
+                    }
+                }
+                return result + 'e';
+            }
+        }
+        throw "unexpected null";
+    } else {
+        throw "unexpected type " + type;
+    }
+};
 
 function checkedIndexOf(s, c) {
-	var result = s.indexOf(c);
-	if (result < 0) {
-		throw "expected a " + c;
-	}
-	return result;
+    var result = s.indexOf(c);
+    if (result < 0) {
+        throw "expected a " + c;
+    }
+    return result;
+}
+
+function decodeInt(s) {
+    s = s.substring(1);
+    var e = checkedIndexOf(s, 'e');
+    return [parseInt(s.substring(0, e), 10), s.substring(e + 1)];
+}
+
+function decodeString(s) {
+    var e = checkedIndexOf(s, ':'),
+        len = parseInt(s.substring(0, e), 10),
+        startOfString = e + 1,
+        endOfString = startOfString + len;
+    return [s.substring(startOfString, endOfString), s.substring(endOfString)];
+}
+
+// Predeclaration to make jslint happy.
+var decode2;
+
+function decodeList(s) {
+    s = s.substring(1);
+    var a = [], s2;
+    while (true) {
+        if (s.length === 0) {
+            throw "end of input while looking for 'e'";
+        }
+        if (s.charAt(0) === 'e') {
+            return [a, s.substring(1)];
+        } else {
+            s2 = decode2(s);
+            a.push(s2[0]);
+            s = s2[1];
+        }
+    }
+}
+
+function decodeDictionary(s) {
+    s = s.substring(1);
+    var a = {}, s2, k;
+    while (true) {
+        if (s.length === 0) {
+            throw "end of input while looking for 'e'";
+        }
+        if (s.charAt(0) === 'e') {
+            return [a, s.substring(1)];
+        } else {
+            s2 = decode2(s);
+            k = s2[0];
+            s = s2[1];
+            s2 = decode2(s);
+            a[k] = s2[0];
+            s = s2[1];
+        }
+    }
 }
 
 function decode2(s) {
-	if ('string' !== typeof s || s.length < 1) {
-		throw "expected a non-empty string";
-	}
-	var c = s.charAt(0);
-	if (c === 'i') {
-		s = s.substring(1);
-		var e = checkedIndexOf(s, 'e');
-		return [parseInt(s.substring(0, e), 10), s.substring(e+1)];
-	} else if (c >= '0' && c <= '9') {
-		e = checkedIndexOf(s, ':');
-		var len = parseInt(s.substring(0, e), 10);
-		var startOfString = e + 1;
-		var endOfString = startOfString + len;
-		return [s.substring(startOfString, endOfString), s.substring(endOfString)];
-	} else if (c == 'l') {
-		s = s.substring(1);
-		var a = [];
-		while ( true ) {
-			if (s.length == 0) {
-				throw "end of input while looking for 'e'";
-			}
-			if (s.charAt(0) == 'e') {
-				return [a, s.substring(1)];
-			} else {
-				var s2 = decode2(s);
-				a.push(s2[0]);
-				s = s2[1];
-			}
-		} 
-	} else if (c == 'd') {
-		s = s.substring(1);
-		var a = {};
-		while ( true ) {
-			if (s.length == 0) {
-				throw "end of input while looking for 'e'";
-			}
-			if (s.charAt(0) == 'e') {
-				return [a, s.substring(1)];
-			} else {
-				var s2 = decode2(s);
-				var k = s2[0];
-				s = s2[1];
-				s2 = decode2(s);
-				a[k] = s2[0];
-				s = s2[1];
-			}
-		} 
-	} else {
-		throw "unexpected character " + c;
-	}
+    if ('string' !== typeof s || s.length < 1) {
+        throw "expected a non-empty string";
+    }
+    var c = s.charAt(0);
+    if (c === 'i') {
+        return decodeInt(s);
+    } else if (c >= '0' && c <= '9') {
+        return decodeString(s);
+    } else if (c === 'l') {
+        return decodeList(s);
+    } else if (c === 'd') {
+        return decodeDictionary(s);
+    } else {
+        throw "unexpected character " + c;
+    }
 }
 
 exports.decode2 = decode2;
 
 exports.decode = function decode(s) {
-	var result = decode2(s);
-	var leftOver = result[1];
-	if (leftOver !== '') {
-		throw "'characters left over at end of decode: '" + leftOver + "'";
-	}
-	return result[0];
-}
+    var result = decode2(s),
+        leftOver = result[1];
+    if (leftOver !== '') {
+        throw "'characters left over at end of decode: '" + leftOver + "'";
+    }
+    return result[0];
+};
--- tests/test.js
+++ tests/test.js
@@ -5,41 +5,41 @@ var bencode = require('../bencode');
 // Test bencode
 
 function assertSame(a, b) {
-	var sa = JSON.stringify(a);
-	var sb = JSON.stringify(b);
-	sys.puts(sa + ' === ' + sb);
-	assert.ok(sa === sb);
+    var sa = JSON.stringify(a),
+        sb = JSON.stringify(b);
+    sys.puts(sa + ' === ' + sb);
+    assert.ok(sa === sb);
 }
 
 function testBencode() {
-	var cases = [
-		// Integers
-		['i0e', 0], ['i1e', 1], ['i10e', 10], ['i-10e', -10],
-		// Strings
-		['0:', ''], ['1:a', 'a'], ['10:abcdefghij', 'abcdefghij'],
-		// Arrays
-		['le', []], ['li0ei1ee', [0, 1]], ['l4:spami42ee', ['spam', 42]],
-		// Dictionaries
-		['de', {}], ['d3:bar4:spam3:fooi42ee', {'bar':'spam', 'foo': 42}]
-		];
-	for (var i = 0, casesLen = cases.length; i < casesLen; i++) {
-		var aCase = cases[i];
-		var encoded = aCase[0];
-		var decoded = aCase[1];
-		var benDecoded = bencode.decode(encoded);
-		var benEncoded = bencode.encode(decoded);
-		assertSame(encoded, benEncoded);
-		assertSame(decoded, benDecoded);
-	}
+    var cases = [
+        // Integers
+        ['i0e', 0], ['i1e', 1], ['i10e', 10], ['i-10e', -10],
+        // Strings
+        ['0:', ''], ['1:a', 'a'], ['10:abcdefghij', 'abcdefghij'],
+        // Arrays
+        ['le', []], ['li0ei1ee', [0, 1]], ['l4:spami42ee', ['spam', 42]],
+        // Dictionaries
+        ['de', {}], ['d3:bar4:spam3:fooi42ee', {'bar': 'spam', 'foo': 42}]
+    ], i, casesLen, aCase, encoded, decoded, benDecoded, benEncoded;
+    for (i = 0, casesLen = cases.length; i < casesLen; i = i + 1) {
+        aCase = cases[i];
+        encoded = aCase[0];
+        decoded = aCase[1];
+        benDecoded = bencode.decode(encoded);
+        benEncoded = bencode.encode(decoded);
+        assertSame(encoded, benEncoded);
+        assertSame(decoded, benDecoded);
+    }
 }
 
 function tests() {
-	try {
-		testBencode();
-	} catch(myError) {
-		sys.puts("Exception: " + JSON.stringify(myError));
-		throw myError;
-	}
+    try {
+        testBencode();
+    } catch (myError) {
+        sys.puts("Exception: " + JSON.stringify(myError));
+        throw myError;
+    }
 }
 
 tests();
